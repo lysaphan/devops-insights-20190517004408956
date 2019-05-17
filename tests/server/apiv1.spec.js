20ec5e6944d26433a1f7e0cca548a013e85d78f6
Lysa Phan <lysa.grass@gmail.com>
2019-05-17 02:38:33 +0000

    Edit the unit test

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -29,8 +29,7 @@
 
   describe('Get Weather', function() {
 
-    //it('with without zip code', function() {
-    it('with without city name', function() {
+    it('with without zip code', function() {
       reqMock = {
         query: {
 
@@ -42,8 +41,7 @@
       assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
     });
 
-    //it('with valid zip code and error from request call', function() {
-    it('with valid city name and error from request call', function() {
+    it('with valid zip code and error from request call', function() {
       reqMock = {
         query: {
           zip: 3204
@@ -62,8 +60,7 @@
       assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    //it('with incomplete zip code', function() {
-    it('with incomplete city name', function() {
+    it('with incomplete zip code', function() {
       reqMock = {
         query: {
           zip: 3204
@@ -82,7 +79,6 @@
       assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    //it('with valid zip code', function() {
     it('with valid zip code', function() {
       reqMock = {
         query: {
